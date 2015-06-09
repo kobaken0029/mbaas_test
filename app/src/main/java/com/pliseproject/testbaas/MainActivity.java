16443910bb3604d994683bf9f325103e8bd0c3ab
Kento Kobayashi <kobayashi_kento@nomadinc.co.jp>
2015-06-09 21:21:48 +0900

    refactoring

--- app/src/main/java/com/pliseproject/testbaas/MainActivity.java
+++ app/src/main/java/com/pliseproject/testbaas/MainActivity.java
@@ -1,35 +1,40 @@
 package com.pliseproject.testbaas;
 
-import android.app.AlertDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.nifty.cloud.mb.*;
 
 import java.util.List;
-import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
-    TextView textView;
-    Button button;
+    /** NIFTY Cloud のデータストアのクラス名 */
+    private static final String NIFTY_DATA_CLASS_NAME = "TestClass";
+
+    /** NIFTY Cloud のデータストアのレコード名 */
+    private static final String NIFTY_DATA_RECORD_NAME = "message";
+
+    /** NIFTY Cloud のアプリケーションキー */
+    private static final String NIFTY_APP_KEY = "d5f48e68422ad7c65227ff2444fb327e5956fef5dabd239183df985f180adaa9";
+
+    /** NIFTY Cloud のクライアントキー */
+    private static final String NIFTY_CLIENT_KEY = "e86658f1c52fdabe3722911706e653f38b3f669e14144ebdc2e8fd30fe3f1ae3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((Button) findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NCMBQuery<NCMBObject> query = NCMBQuery.getQuery("TestClass");
-//                query.whereEqualTo("message", "Hello, NCMB!");
+                NCMBQuery<NCMBObject> query = NCMBQuery.getQuery(NIFTY_DATA_CLASS_NAME);
                 try {
                     query.setSkip((int) (Math.random() * query.count()));
                 } catch (Exception e) {
@@ -37,43 +42,28 @@ public class MainActivity extends ActionBarActivity {
                 query.findInBackground(new FindCallback<NCMBObject>() {
                     @Override
                     public void done(List<NCMBObject> result, NCMBException e) {
-//                        if (result.isEmpty() != true) {
-                            ((TextView) findViewById(R.id.text)).setText(result.get(0).getString("message"));
-//                            dispMessage(result.get(0));
-//                        } else {
-//                            PostData();
-//                        }
+                        ((TextView) findViewById(R.id.text))
+                                .setText(result.get(0).getString(NIFTY_DATA_RECORD_NAME));
                     }
                 });
 
             }
         });
 
-        ((Button) findViewById(R.id.submit)).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PostData();
             }
         });
 
-        NCMB.initialize(this, "d5f48e68422ad7c65227ff2444fb327e5956fef5dabd239183df985f180adaa9",
-                "e86658f1c52fdabe3722911706e653f38b3f669e14144ebdc2e8fd30fe3f1ae3");
-
-
+        NCMB.initialize(this, NIFTY_APP_KEY, NIFTY_CLIENT_KEY);
     }
 
     private void PostData() {
-        NCMBObject TestClass = new NCMBObject("TestClass");
-        TestClass.put("message", ((EditText)findViewById(R.id.edit)).getText().toString());
+        NCMBObject TestClass = new NCMBObject(NIFTY_DATA_CLASS_NAME);
+        TestClass.put(NIFTY_DATA_RECORD_NAME, ((EditText) findViewById(R.id.edit)).getText().toString());
         TestClass.saveInBackground();
-    }
-
-    private void dispMessage(NCMBObject message) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-
-        alertDialogBuilder.setTitle("データ取得");
-        alertDialogBuilder.setMessage(message.getString("message"));
-        alertDialogBuilder.show();
     }
 
     @Override
